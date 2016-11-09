d526ffa552d184c464c0d69186358a84fb683673
Georg Groenendaal <georg.groenendaal@gmail.com>
2016-11-09 18:12:12 +0100

    Added some working pyserial code, turns led 13 on/off

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,22 @@
 # Main python
+import serial
+import time
+import io
 
 if __name__ == "__main__":
-    print("Hello world")
+
+
+    ser = serial.Serial(
+        port='COM3',
+        baudrate=9600,
+        timeout=0.1
+    )
+
+    while 1:
+        string = bytes.fromhex('024203');
+        ser.write(string)
+        time.sleep(1)
+        string = bytes.fromhex('024303');
+        ser.write(string)
+        print(ser.read(3).decode('utf8'))
+        time.sleep(1)
