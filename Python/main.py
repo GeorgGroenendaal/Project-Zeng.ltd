cc0267634259fe73bc6cad5fe1696880bbdef0f9
Georg Groenendaal <georg.groenendaal@gmail.com>
2016-11-10 22:07:21 +0100

    Added basic python float decoder, simple temp measurement program

--- Python/main.py
+++ Python/main.py
@@ -1,22 +1,47 @@
 # Main python
 import serial
 import time
-import io
+import threading
+import struct
+
+cmd = []
+stop = False
+
+def decode():
+    data = cmd[2:6]
+    by = b''.join(data)
+    temp = struct.unpack('<f', by)
+    print("Temperatuur is {0:.1f} graden Celcius".format(temp[0]))
+
+# Send hex instruction to ard
+def send_instruction(instr):
+    cmd = '02' + instr + '03'
+    ser.write(bytes.fromhex(cmd))
+
+# Listens to incomming serial connection
+def listen():
+    while stop == False:
+        char = ser.read(1)
+        if (bytes.fromhex('02') == char):
+            cmd.append(char)
+        elif (bytes.fromhex('03') == char):
+            cmd.append(char)
+            decode()
+        else:
+            cmd.append(char)
 
 if __name__ == "__main__":
-
-
     ser = serial.Serial(
         port='COM3',
         baudrate=9600,
-        timeout=0.1
+        timeout=1
     )
 
-    while 1:
-        string = bytes.fromhex('024203');
-        ser.write(string)
+    time.sleep(2)
+    t = threading.Thread(target=listen)
+    t.start()
+    for i in range(0,10):
+        send_instruction('42')
         time.sleep(1)
-        string = bytes.fromhex('024303');
-        ser.write(string)
-        print(ser.read(3).decode('utf8'))
-        time.sleep(1)
+        cmd = []
+    stop = True
